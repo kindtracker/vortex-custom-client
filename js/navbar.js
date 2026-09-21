69ace6b3714bc536a786b75660c1e6749f8c721b
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-09-20 21:08:45 -0300

    make navbar better

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,14 @@ async function LoadNavBar() {
   const Content = await Response.text();
 
   document.getElementById("NavBar").innerHTML = Content;
+
+  const CurrentPath = window.location.pathname;
+
+  document.querySelectorAll(".NavBarAction a").forEach((Link) => {
+    if (Link.pathname === window.location.pathname) {
+      Link.classList.add("Active");
+    }
+  });
 }
 
 LoadNavBar();
